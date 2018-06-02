974511bce432b46ca5f7e0d71c5068c11cc22a30
bigbrother13 <gritskevich-91@mail.ru>
2018-06-03 01:40:20 +0300

    First comit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,25 +1,36 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  
+
+  def search
+    if params[:search].present?
+      @movies = Movie.search(params[:search])
+    else
+      @movies = Movie.all
+    end
+  end
+
   def index
     @movies = Movie.all
   end
 
-
   def show
+    @reviews = Review.where(movie_id: @movie.id).order("created_at DESC")
+
+    if @reviews.blank?
+      @avg_review = 0
+    else
+      @avg_review = @reviews.average(:rating).to_f.round(2)
+    end
   end
 
- 
   def new
     @movie = current_user.movies.build
   end
 
-  
   def edit
   end
 
- 
   def create
     @movie = current_user.movies.build(movie_params)
 
@@ -55,13 +66,11 @@ class MoviesController < ApplicationController
   end
 
   private
-    
     def set_movie
       @movie = Movie.find(params[:id])
     end
 
-    
     def movie_params
-      params.require(:movie).permit(:title, :description, :movie_length, :derector, :rating, :image)
+      params.require(:movie).permit(:title, :description, :movie_length, :director, :rating, :image)
     end
 end
